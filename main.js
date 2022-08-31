57497c9684152a88170d6129c691e328a673d5c8
clauds-H <claudiush96@gmx.de>
2022-08-31 15:13:57 +0200

    create_valid_array

---
+++ main.js
@@ -0,0 +1,34 @@
+// String wird rechenbar gemacht
+function splitTerm(str){
+
+    let validOp = '+-/*'; //['+', '-', '/', '*'];
+
+    let arr = str.split(/([-+*\/])/).filter(y => y !== '');
+    
+    console.log(arr);
+ 
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++){
+        if(i === 0 && arr[i] === '-'){
+            newArr.push(0-arr[1]);
+            i++;
+        }
+        else if((i > 0 && arr[i-1] === '-' && arr[i] === '-')){
+            newArr.push(Math.abs(arr[i+1]));
+            i++;
+        }
+        else if(i > 0 && arr[i-1].indexOf(validOp) && arr[i] === '-'){
+            newArr.push(0-arr[i+1]);
+            i++;
+        }
+        else{
+            newArr.push(arr[i]);
+        }
+    }
+    console.log(newArr);
+    return newArr.map( x => parseInt(x) ? parseInt(x) : x);
+}
+
+//testing
+let str = '-3*-5+21+-324/275';
+console.log(splitTerm(str));
