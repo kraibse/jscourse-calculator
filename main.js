b7c681bdfa841abcd39786b9c2e734bedffebd0b
m3Nemo <112379637+m3Nemo@users.noreply.github.com>
2022-09-01 16:21:48 +0200

    Punkt vor Strich funktioniert. Klammern nicht.

--- main.js
+++ main.js
@@ -18,11 +18,11 @@ function calculate (splitTerm) {
                 break;
 
             case '*':
-                erg = substract(erg, splitTerm[i+1]);
+                erg = multiply(erg, splitTerm[i+1]);
                 break;
 
             case '/':
-                erg = substract(erg, splitTerm[i+1]);
+                erg = divide(erg, splitTerm[i+1]);
                 break;
 
         }
@@ -85,6 +85,74 @@ function splitTerm(str){
     console.log(newArr);
     return newArr.map( x => parseInt(x) ? parseInt(x) : x);
 }
+
+
+function calcLogic (splitTerm) {
+
+    let mainArr = structuredClone(splitTerm);
+
+    // Iteriere durch alle Elemente zur Klammeraufloesung
+    for (let i = 0; i < mainArr.length; i++) {
+        
+         // Berechnet die Rechenaufgabe in der Klammer.]]]
+        if (mainArr[i] == '(') {
+
+            let splitArr = structuredClone(mainArr);
+
+            for (let j = 0; j < (i + 1); j++) {
+                splitArr.shift();
+            };
+
+            let result = calcLogic(splitArr);
+
+            //Zwischenspeichern des vorherigen Rechenarrays
+            let ersterTeil = [];
+            for (let j = 0; j > mainArr.length; j++) {
+
+                if (mainArr[j] == '(') {
+                    ersterTeil = mainArr.splice[0, j];
+                    break;
+                };
+            };
+
+            // Ersetzt Ergebnis im urspruenglichen Array
+            for (let j = 0; j < (i + 1); j++) {
+
+                if (mainArr[j] == ')') {
+                    mainArr[j] = result;
+                    mainArr = ersterTeil.concat(mainArr);
+                } else {
+                    splitArr.shift();
+                };
+            };
+            // Rekursiver Aufruf des Arrays ein Index nach gefundener oeffnender Klammer 
+        } else if (mainArr[i] == ')') {
+
+            let splitArr = structuredClone(mainArr);
+        
+            for (let j = splitArr.length; j > (i - 1); j--) {
+                splitArr.pop();
+            };
+        };   
+    };
+
+    // 2. Iteration fuer mal und geteilt
+    for (let i = 0; i < mainArr.length; i++) {
+
+        if (mainArr[i] == '*' || mainArr[i] == '/') {
+
+            mainArr[i+1] = calculate([mainArr[i-1], mainArr[i], mainArr[i+1]]);
+            mainArr[i] = '+';
+            mainArr[i-1] = 0;
+        };
+    };
+
+    // 3. Berechnung der restlichen addition und substraktion
+    return calculate(mainArr);
+}
+//testing: Mal/Geteilt funktioniert, Klammersetzung muss noch gefixed werden
+testArray = [1, '*', 2, '+', '(', 3, '/', 3, ')'];
+document.body.innerHTML = calcLogic(testArray);
 
 //testing
 let str = '-4--3';
