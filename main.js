16c6a1863652d6c4278b2c8551b5c2d7c1838e92
clauds-H <claudiush96@gmx.de>
2022-09-01 10:07:06 +0200

    KlammerFix, FloatFix

--- main.js
+++ main.js
@@ -60,32 +60,41 @@ function splitTerm(str){
 
     let validOp = '+-/*'; //['+', '-', '/', '*'];
 
-    let arr = str.split(/([-+*\/])/).filter(y => y !== '');
+    let arr = str.split(/([-+*\/()])/).filter(y => y !== '');
     
     console.log(arr);
  
     let newArr = [];
+
     for(let i = 0; i < arr.length; i++){
-        if(i === 0 && arr[i] === '-'){
+        if((i === 0) && (arr[i] === '-')){
             newArr.push(0-arr[1]);
             i++;
         }
-        else if((i > 0 && (arr[i+1] === '-') && (arr[i] === '-'))){
+        else if(((i > 0) && (arr[i+1] === '-') && (arr[i] === '-'))){
             newArr.push('+');
             i++;
         }
-        else if(i > 0 && arr[i-1].indexOf(validOp) && arr[i] === '-'){
+        else if((i > 0) && arr[i+1] === '(' && arr[i] === '-'){
+            newArr.push('-','(');
+            i++;
+        }
+        else if((i > 0) && arr[i-1].indexOf(validOp) && (arr[i] === '-')){
             newArr.push(0-arr[i+1]);
             i++;
         }
+
         else{
             newArr.push(arr[i]);
         }
     }
+
     console.log(newArr);
-    return newArr.map( x => parseInt(x) ? parseInt(x) : x);
+
+
+    return newArr.map( x => parseInt(x) ? parseFloat(x) : x);
 }
 
 //testing
-let str = '-4--3';
+let str = '1.5-((4*-3.5)+(1*2))';
 console.log(calculate(splitTerm(str)));
