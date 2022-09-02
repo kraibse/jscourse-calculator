353a24a0f931e600450d128292cd64d3a515b211
Claudius H <55752864+clauds-H@users.noreply.github.com>
2022-09-02 15:16:46 +0200

    Update main.js
    aus ButtonFunktionen

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ function calculate (splitTerm) {
 
     let erg = splitTerm[0];
 
-    for(let i = 1; i <= splitTerm.length; i+=2){
+    for(let i = 1; i <= splitTerm.length; i+=2) {
 
-        switch (splitTerm[i]){
+        switch (splitTerm[i]) {
 
             case '+':
                 erg = add(erg, splitTerm[i+1]);
@@ -18,11 +18,11 @@ function calculate (splitTerm) {
                 break;
 
             case '*':
-                erg = substract(erg, splitTerm[i+1]);
+                erg = multiply(erg, splitTerm[i+1]);
                 break;
 
             case '/':
-                erg = substract(erg, splitTerm[i+1]);
+                erg = divide(erg, splitTerm[i+1]);
                 break;
 
         }
@@ -50,12 +50,13 @@ function multiply (a, b) {
 function divide (a, b) {
 
     if (b == 0) {
-        return "Chuck Norris kann durch Null teilen.";
+        return "Nur Chuck Norris kann durch Null teilen.";
     };
     return a / b;
 }
 
 // String wird rechenbar gemacht
+// str -> arr
 function splitTerm(str){
 
     let validOp = '+-/*'; //['+', '-', '/', '*'];
@@ -66,35 +67,64 @@ function splitTerm(str){
  
     let newArr = [];
 
-    for(let i = 0; i < arr.length; i++){
-        if((i === 0) && (arr[i] === '-')){
+    for(let i = 0; i < arr.length; i++) {
+        //erstes Element negativ
+        if ( (i === 0) && (arr[i] === '-')) {
             newArr.push(0-arr[1]);
             i++;
         }
-        else if(((i > 0) && (arr[i+1] === '-') && (arr[i] === '-'))){
+        //negativ nach Klammer
+        else if ( arr[i] === '(' && arr[i+1] === '-') {
+            newArr.push('(', 0-arr[i+2]);
+            i+=2;
+        }
+        //Minus Minus = Plus
+        else if ( (i > 0) && (arr[i] === '-') && (arr[i+1] === '-')) {
             newArr.push('+');
             i++;
         }
-        else if((i > 0) && arr[i+1] === '(' && arr[i] === '-'){
+        //negaives Vorzeichen vor Klammer --> -(
+        else if ( (i > 0) && arr[i] === '-' && arr[i+1] === '(') {
             newArr.push('-','(');
             i++;
         }
-        else if((i > 0) && arr[i-1].indexOf(validOp) && (arr[i] === '-')){
-            newArr.push(0-arr[i+1]);
-            i++;
+        //negative Zahl --> *-4
+        else if ( (i > 0) && arr[i].indexOf(validOp) && (arr[i+1] === '-')) {
+            newArr.push(arr[i], 0-arr[i+2]);
+            i+=2;
         }
 
-        else{
+        else {
             newArr.push(arr[i]);
         }
     }
 
     console.log(newArr);
 
-
-    return newArr.map( x => parseInt(x) ? parseFloat(x) : x);
+    return newArr.map( x => parseFloat(x) ? parseFloat(x) : x);
 }
 
+// sendet Input an Funktionen und schreibt Ergebnis in Input
+function subm() {
+    event.preventDefault(); //veraltet aber funktioniert
+    let inputField = document.getElementById('equation');
+    let history = document.getElementById('results');
+    let textStr = inputField.value;
+
+    //hier wird gerechnet
+    let result = calculate(splitTerm(textStr));
+    console.log(result);
+    
+    inputField.value = result;
+    history.appendChild(document.createElement('p')).textContent = textStr + ' = ' +result;
+    history.scrollTop = history.scrollHeight;
+}
+
+
+
 //testing
-let str = '1.5-((4*-3.5)+(1*2))';
-console.log(calculate(splitTerm(str)));
+//let str = '1.5-((-4*-3.5)+(1*2))';
+//console.log(calculate(splitTerm(str)));
+
+
+
